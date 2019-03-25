a60e94d4e8ff4be47585feec82524854bc863b8c
OJ3rillz <timitrilli@gmail.com>
2019-03-25 22:41:37 +0100

    add updates to node modulus dir and others

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  var products
   res.render('shop/index', { title: 'Shopping Cart' });
 });
 
--- shopping-cart/seed/product-seeder.js
+++ shopping-cart/seed/product-seeder.js
@@ -1,6 +1,8 @@
 var Product = require('../models/product');
 
-var mongoose = require()
+var mongoose = require('mongoose');
+
+mongoose.connect('localhost:27017/shopping');
 
 var products = [
    new Product({
@@ -26,9 +28,18 @@ new Product({
       title: 'Bracelet',
       description: 'Awesome Bracelet!!!!',
       price: 70
- }),
+ })
 ];
-
+var done = 0;
 for (var i = 0; i < products.length; i++) {
-      products[i].save();
+      products[i].save(function(err, result){
+      done++;
+      if (done === products.length) {
+           exit();
+      }
+      });
+}
+
+function exit() {
+     mongoose.disconnect();
 }
