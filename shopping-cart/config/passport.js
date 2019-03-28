b275d0d10aad3f7b3a7aeffb7425067d310f7cff
OJ3rillz <timitrilli@gmail.com>
2019-03-28 14:23:24 +0100

    install express-validators

--- shopping-cart/config/passport.js
+++ shopping-cart/config/passport.js
@@ -2,7 +2,7 @@ var passport = require('passport');
 var User = require('../models/user');
 var LocalStrategy = require('passport-local').Strategy;
 
-passport.serializeUser(function(user, done){
+passport.serializeUser(function (user, done){
            done(null, user.id);
 });
 
@@ -16,7 +16,7 @@ passport.use('local.signup', new LocalStrategy({
       usernameField: 'email',
       passwordField: 'password',
       passReqToCallback: true
-}, function(req, email, password, done) {
+}, function (req, email, password, done) {
       User.findOne({'email': email}, function(err, user){
             if (err) {
                   return done(err);
